2cdcff31352865669b6ab5b720adf544f2dc74e6
Ian <ianmat55@gmail.com>
2021-09-07 08:08:34 -0700

    init

---
+++ socket_tutorial/server.py
@@ -0,0 +1,26 @@
+import socket
+
+#socket chat between client(s) and server
+
+server = '127.0.0.1' # Standard loopback interface addy (localhost)
+port = 65432 # Non-privileged ports are > 1023
+users = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket(address family for IPv4, socket type for TCP)
+s.bind((server, port)) #bind, associates socket with specific network and port number
+s.listen(5) #queus up to 5 connect requests
+
+while True:
+	clientcon, addr = s.accept() 
+	users.append(clientcon)
+
+	with clientcon:
+		print(f"Connection from {addr}")
+		clientcon.send(bytes(f"Welcome to the server", "UTF-8"))
+		while True:
+			data = clientcon.recv(2048)
+			print(data)
+
+			if not data:
+				break
+			clientcon.sendall(data)
