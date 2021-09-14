a9dd5ce47046630920adc9d34596cbb1f507d47d
Ian <ianmat55@gmail.com>
2021-09-13 23:26:05 -0700

    structure requires that ip and port are not tied to the same class but used as an external value outside of init during connect/listen method since both client/server are on the same computer

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
 import pytest
-from base_socket import Client
+import base_socket as base
 
 ip = '127.0.0.1' #localhost
 port = 6500
 
+#set up and tear down for test runs
 @pytest.fixture
-def client():
-	Client = Client(ip,port)
-	return Client
+def socket():
+	s = base.Socket()
+	return s
 
+@pytest.fixture
+def server():
+	pass
--- tests/test_socket.py
+++ tests/test_socket.py
@@ -1,5 +1,19 @@
 import pytest
-from conftest import client
+#https://pypi.org/project/pytest-socket/
+#Testing functionality for base client class
 
-def test_sanity(client):
-	assert client != None
+def test_sanity(socket):
+	assert socket.ip == '127.0.0.1'
+
+def test_IPv(socket):
+	assert str(socket.con.family) == "AddressFamily.AF_INET"
+
+
+def test_TCP(socket):
+	assert str(socket.con.type) == "SocketKind.SOCK_STREAM"
+
+def test_recv(socket):
+	pass
+
+def test_shutdown(socket):
+	pass
