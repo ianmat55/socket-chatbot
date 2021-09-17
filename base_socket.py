03dcdc2e6789a99ecc2971466dd461da2ce005fb
Ian <ianmat55@gmail.com>
2021-09-17 08:46:58 -0700

    ascii art title added

--- base_socket.py
+++ base_socket.py
@@ -1,12 +1,25 @@
+# Built with python version 3.8.10
+# email: ianmat55@gmail.com
+
 import socket, threading
+import ux
+
+############################################ CLIENT CLASS ################################################
 
 class Client:
 	def __init__(self, ip, port, nick=None):
-		self.con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.con = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 		self.ip = ip
 		self.port = port
-		self.nick = nick
+
+		# username of client, need input when integrating class, can't put in init or 
+		# itll run on import
+		self.nick = nick 
+		print(ux.border)
+		print(ux.title)
+		print(ux.border)
 	
+	# turned threading into a function because TDD principles?
 	def thread(self, func, params=None):
 		if params == None:
 			conn = threading.Thread(target=func)
@@ -15,29 +28,39 @@ class Client:
 			conn = threading.Thread(target=func, args=(params))
 			conn.start()
 
-	# open a txt file, send the contents
-	# def read_file(self, file):
-	# 	with open(str(file)) as f:
-	# 		lines = f.readlines()
-	# 		for line in lines:
-	# 			self.con.send(line.encode("UTF-8"))
+	# open a txt file, send the contents. Texts are in 'texts' folder
+	def read_file(self, file):
+		with open(str(file)) as f:
+			lines = f.readlines()
+			for line in lines:
+				self.con.send(line.encode("UTF-8"))
 
 	def connect(self):
-		self.con.connect((self.ip, self.port))
-		self.thread(self.recv_msg)
-		print(f"* Welcome {self.nick}")
+		try:
+			self.con.connect((self.ip, self.port))
+			self.thread(self.recv_msg)
+			print(f"* Welcome {self.nick}")
+		except:
+			print('Could not connect to server')
 
 
 	def send_msg(self):
 		while True:
-			msg = input("")
-			if msg == 'close()':
-				self.con.close()
-			else:
-				self.con.send(f"[{self.nick}] {msg}".encode("UTF-8"))
+			try:
+				msg = input("") # input needs to be blank for formatting in CLI, find a workaround later
 
+				# cmd line functions (close, send contents of text file, etc)
+				if msg == 'close()':
+					self.con.close()
+				else:
+					self.con.send(f"[{self.nick}] {msg}".encode("UTF-8"))
+
+			except:
+				print("connection failed")
+				break
 			
 	def recv_msg(self):
+
 		#thread to send messages if message is typed
 		self.thread(self.send_msg)
 
@@ -54,18 +77,14 @@ class Client:
 				self.con.close()
 				break	
 
-	# def shutdown(self, client_sock=None):
-	# 	if client_sock != None:
-	# 		client_sock.shutdown(socket.SHUT_RDWR)
-	# 		client_sock.close()
-	# 	self.con.shutdown(socket.SHUT_RDWR)
-	# 	self.con.close()
-	
 	#print info
 	def get_constants(self, pref):
 		return dict((getattr(self.con,n), n) for n in dir(self.con) if n.startswith(pref))
 
 	def __repr__(self):
+
+		# object attributes for socket object, easier to see if strings are in the attribute
+		# since actual attribute name is long
 		families = str(self.get_constants('AF_'))
 		types = str(self.get_constants('SOCK_'))
 		protocols = str(self.get_constants("IPPROTO_"))
@@ -76,29 +95,30 @@ class Client:
 
 
 
-#Server class
+############################################ SERVER CLASS #################################################
+
 class Server(Client):
 	def __init__(self, ip, port, users=None):
 		super().__init__(ip, port)
+
+		# Not good to put a mutable object in init, could cause problems with different versions 
 		if users == None:
 			self.users = {}
 		else:
 			self.users = users
 	
 	def listen(self):
-		self.con.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.con.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Reuse addr if con reopened
 		self.con.bind((self.ip, self.port))
 		self.con.listen(5)
 	
 	def accept(self):
 		self.con.accept()
 	
-
 	def broadcast(self, msg, client_sock=None):
 		for client in self.users.keys():
 			if client != client_sock:
 				client.send(msg.encode("UTF-8"))	
-
 	
 	# def transcribe(self, msg):
 	# 	with open('transcription.txt', 'w') as trans:
@@ -120,7 +140,9 @@ class Server(Client):
 				break	
 
 	def start(self):
-		print("Initializing server...")
+
+		# Add onload string to include server commands and nice start-up message
+
 		while True:
 			try:
 				client_sock, addr = self.con.accept()
