20d5f885c115b1e0517f6592999b82739d54ea1d
Ian <ianmat55@gmail.com>
2021-09-17 09:30:33 -0700

    user inputs ip in start scripts. Want to integrate in actual class later

--- start_client.py
+++ start_client.py
@@ -1,10 +1,13 @@
 from base_socket import Client 
 import threading
 
-ip = '127.0.0.1'
 port = 6500
 
 def main():
+	ip_enter = input("Enter ip ('local' for localhost): ")
+	if ip_enter == 'local':
+		ip = '127.0.0.1'
+	else: ip = ip_enter
 	user = input('what is your name? ')
 	client = Client(ip, port, user)
 	client.connect()
--- start_server.py
+++ start_server.py
@@ -1,9 +1,13 @@
 from base_socket import Server
 
-ip = '127.0.0.1'
+# ip = '127.0.0.1'
 port = 6500
 
 def main():
+	ip_enter = input("Enter ip ('local' for localhost): ")
+	if ip_enter == 'local':
+		ip = '127.0.0.1'
+	else: ip = ip_enter
 	server = Server(ip, port)
 	server.listen()
 	server.start()
